5e22e2dcb7da4f495c2d550391fe19bcd7c84aa6
findstr <findstr@sina.com>
2017-05-08 21:59:57 +0800

    only layout ui

---
+++ client/Assets/Scripts/main.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
