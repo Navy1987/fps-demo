043048d782e2052d4817c1d39d45da25088ff10b
findstr <findstr@sina.com>
2017-05-10 16:31:08 +0800

    remove meta file ignore

---
+++ client/Assets/Scripts/Login.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Button btn;
+		btn = GetComponent<Button>();
+		btn.onClick.AddListener(OnButtonClick);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnButtonClick() {
+		Debug.Log("Hello");
+	}
+}
