8e4314c4e3cc72086d056b71cfeba99d1d7872f2
findstr <findstr@sina.com>
2017-05-10 17:38:30 +0800

    instantiate loginwnd

--- client/Assets/Scripts/LoginWnd.cs
+++ client/Assets/Scripts/LoginWnd.cs
@@ -3,21 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Login : MonoBehaviour {
-
+public class LoginWnd : MonoBehaviour {
+	public Button login_btn;
 	// Use this for initialization
 	void Start () {
-		Button btn;
-		btn = GetComponent<Button>();
-		btn.onClick.AddListener(OnButtonClick);
+
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-	}
-
-	void OnButtonClick() {
-		Debug.Log("Hello");
 	}
 }
--- client/Assets/Scripts/main.cs
+++ client/Assets/Scripts/main.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class main : MonoBehaviour {
-
+	public GameObject loginwnd;
 	// Use this for initialization
 	void Start () {
-		
+		Instantiate(loginwnd, new Vector3(0, 0, 0), Quaternion.identity);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
