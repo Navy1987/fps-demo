49b29ef80dba1c4ea3b07671b716d963b3957711
findstr <findstr@sina.com>
2017-05-14 21:07:04 +0800

    modify battle broadcast

--- server/gate-src/battle.lua
+++ server/gate-src/battle.lua
@@ -6,10 +6,6 @@ local errno = require "protocol.errno"
 
 local arena = {}
 
-local function close(fd)
-	arena[fd] = nil
-end
-
 local function broadcast(typ, req)
 	for k, v in pairs(arena) do
 		local fd = player.fd(k)
@@ -17,27 +13,40 @@ local function broadcast(typ, req)
 	end
 end
 
-local function join(fd, req)
-	print("join")
+local function close(fd)
 	local uid = player.uid(fd)
+	arena[fd] = nil
+	local req = {
+		uid =  uid,
+		join = 0
+	}
+	server.hookclose(fd, nil)
+	broadcast("a_join", req)
+end
+
+
+local function join(fd, req)
+	local uid = player.uid(fd)
+	print("join", uid, req.join)
+	req.uid = uid
 	if req.join == 0 then
 		server.hookclose(fd, nil)
-		req.uid = uid
-		broadcast("a_join", req)
 		arena[uid] = nil
 	else
 		server.hookclose(fd, close)
 		arena[uid] = true
 		for k, v in pairs(arena) do
 			req.uid = k
-			server.send(fd, "a_join", req);
+			server.send(fd, "a_join", req)
 		end
+		req.uid = uid
 	end
+	broadcast("a_join", req)
 end
 
 local function sync(fd, req)
-	print("sync")
 	local uid = player.uid(fd)
+	print("sync", uid, req.pos.x/100000, req.pos.y/100000, req.pos.z/100000)
 	req.uid = uid
 	broadcast("a_sync", req)
 end
