49ce6c52226ba08918039c85b313b0fb745c917b
findstr <findstr@sina.com>
2017-06-27 12:58:50 +0800

    add map

--- client/Assets/Scripts/Input/InputManager.cs
+++ client/Assets/Scripts/Input/InputManager.cs
@@ -8,14 +8,27 @@ public class InputManager : MonoBehaviour {
 		return 0.0f;
 	}
 	public static float GetMoveZ() {
+#if UNITY_EDITOR
+		return Input.GetAxis("Vertical") * 8 * Time.deltaTime;
+#else
 		return GameInstance.joystick.Move.y * Time.deltaTime;
+#endif
 	}
 	public static float GetTurnX() {
 		return 0.0f;
 	}
 	public static float GetTurnY() {
+#if UNITY_EDITOR
+		float h = Input.GetAxis("Horizontal");
+		if (h < 0.1f && h >-0.1f)
+			return 0.0f;
+		float v = Input.GetAxis("Vertical");
+		float t = Mathf.Atan2(h, v) * Time.deltaTime * 20;
+		return t;
+#else
 		Vector2 move = GameInstance.joystick.Move;
 		return Mathf.Atan2(move.x, move.y);
+#endif
 	}
 	public static bool GetFire1() {
 		return Input.GetButtonDown("Fire1");
